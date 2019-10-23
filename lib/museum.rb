d9be91f32f62914f48cac073be8b48acc8797ce6
Becky Robran <rerobran@gmail.com>
2019-10-23 11:00:06 -0600

    "Adds recommend_exhibits + by_interest"

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,8 +1,38 @@
 class Museum
-  attr_reader :name, :exhibits 
+  attr_reader :name, :exhibits, :patrons, :exhibit_by_interest
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
+    @exhibit_by_interest = {}
   end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    interests = patron.interests
+
+    interests.flat_map do |interest|
+      @exhibits.find_all do |exhibit|
+        exhibit.name == interest
+      end
+    end
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interests(patron)
+
+    recommend_exhibits(patron).reduce({}) do |exhibit_by_interest, exhibit|
+      @exhibit_by_interest[exhibit] = patron
+      @exhibit_by_interest
+    end
+
+  end
+
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -28,9 +28,58 @@ class MuseumTest < MiniTest::Test
   def test_it_can_add_exhibits
     @dmns.add_exhibit(@gems_and_minerals)
     @dmns.add_exhibit(@dead_sea_scrolls)
-    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
     assert_equal 3, @dmns.exhibits.size
     assert_equal true, @dmns.exhibits.include?(@gems_and_minerals)
+    assert_equal true, @dmns.exhibits.include?(@dead_sea_scrolls)
+    assert_equal true, @dmns.exhibits.include?(@imax)
+  end
+
+  def test_it_can_recommend_exhibits
+    bob = Patron.new("Bob", 20)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("IMAX")
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(bob)
+    assert_equal [@imax], @dmns.recommend_exhibits(sally)
+  end
+
+  def test_it_starts_with_no_patrons
+    assert_equal [], @dmns.patrons
+  end
+
+  def test_it_can_add_patrons
+    bob = Patron.new("Bob", 20)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("IMAX")
+    @dmns.admit(bob)
+    @dmns.admit(sally)
+    assert_equal 2, @dmns.patrons.size
+    assert_equal true, @dmns.patrons.include?(bob)
+    assert_equal true, @dmns.patrons.include?(sally)
+  end
+
+  def test_it_can_track_patrons_exhibit_intersts
+    bob = Patron.new("Bob", 20)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("IMAX")
+    sally.add_interest("Dead Sea Scrolls")
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @dmns.admit(bob)
+    @dmns.admit(sally)
+    @dmns.patrons_by_exhibit_interests(bob)
+    @dmns.patrons_by_exhibit_interests(sally)
+    assert_equal [bob, sally], @dmns.exhibit_by_interest[@dead_sea_scrolls]
   end
 
 end
